85e06057ddb569626ff200607d4b365062a63c4d
Anna <ane4ka.hubskaya@gmail.com>
2020-06-11 18:07:41 +0300

    task_2.1-name

---
+++ 2nd lesson/2.1.py
@@ -0,0 +1,10 @@
+print('\t\t\t\t*\t\t\t\t*\t\t\t\t\t* *\t\t\t\t\t*\t\t\t\t*\t\t\t\t\n\t\t\t\t\b*'
+      '\t\t*\t\t\t*\t*\t\t\t\t* *\t*\t\t\t\t*\t\t\t\t\b*\t\t*')
+print('\t\t\t\b* * * * * * * * *\t\t*\t\t*\t\t\t* *\t\t*\t\t\t*\t\t\t\b* * * * * * * * *\n'
+      '\t\t\b*\t\t\t\t\t\t*\t*\t\t\t*\t\t* *\t\t\t*\t\t*\t\t\b*\t\t\t\t\t\t*')
+print('\t\b*\t\t\t\t\t\t\t\t*\t\t\t\t*\t* *\t\t\t\t*\t*\t\b*\t\t\t\t\t\t\t\t*')
+
+
+
+
+
