f338ab1d919bbbd162425c63053429abb2a09492
Anna <ane4ka.hubskaya@gmail.com>
2020-06-14 14:23:39 +0300

    Home work #4

---
+++ 3rd lesson/hw_4.py
@@ -0,0 +1,12 @@
+a = int(input('Class math_1 size is '))
+b = int(input('Class math_2 size is '))
+c = int(input('Class math_2 size is '))
+print('\n')
+x = a + b + c
+if a > 0 and b > 0 and c > 0:
+    if (x % 2) > 0:
+        print('You need to buy', (x // 2) + 1, 'desks')
+    else:
+        print('You need to buy', x // 2, 'desks')
+else:
+    print('Please enter correct data')
