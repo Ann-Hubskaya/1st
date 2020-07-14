802ae4a1f97dee7e6ce29d29aa0df91d44356b7b
Anna <ane4ka.hubskaya@gmail.com>
2020-07-14 15:55:15 +0300

    hw_14_v2

--- 6th lesson/hw_14.py
+++ 6th lesson/hw_14.py
@@ -3,5 +3,16 @@
 # Примечание. Эту задачу можно решить в одну строчку.
 
 import random
-res = len(set([random.randint(1, 12) for _ in range(10)]+[random.randint(1, 12) for _ in range(10)]))
+res = len((set([random.randint(1, 12) for _ in range(10)])^set([random.randint(1, 12) for _ in range(10)])))
 print(res)
+
+# a = [random.randint(1, 12) for _ in range(10)]
+# b = [random.randint(1, 12) for _ in range(10)]
+# print(a)
+# print(b)
+# c = set(a)
+# print(c)
+# d = set(b)
+# print(d)
+# print((c^d))
+# print(len(c^d))
