14d9cb218dc604c180eb7d88fa6b5cb1d1bba8c6
Anna <ane4ka.hubskaya@gmail.com>
2020-07-20 00:39:36 +0300

    hw_4(v3)

--- 3rd lesson/4v2.py
+++ 3rd lesson/4v2.py
@@ -2,5 +2,5 @@ a = int(input('Class math_1 size is '))
 b = int(input('Class math_2 size is '))
 c = int(input('Class math_3 size is '))
 
-n = (a+b+c)//2+(a+b+c)%2
+n = a//2 + a % 2 + b//2 + b % 2 + c//2 + c % 2
 print('You need to buy', n, 'desks')
